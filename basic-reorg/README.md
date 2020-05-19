c1bbeac860661083b97cead8a6bd055b9c57d76a
Larry Ruane <larryruane@gmail.com>
2020-05-19 15:25:17 -0600

    use basic-reorg/generate.sh to regenerate all blocks

--- basic-reorg/README.md
+++ basic-reorg/README.md
@@ -3,11 +3,11 @@
 These block sets are for testing reorgs that don't involve any transactions
 related to the wallet.
 
-- `before-reorg.txt` contains empty blocks 663151 to 663200.
-- `after-small-reorg.txt` contains empty blocks 663151 to 663250, except with
+- `before-reorg.txt` contains real mainnet block 663150 followed by empty blocks 663151 to 663200.
+- `after-small-reorg.txt` contains empty blocks 663195 to 663250, with
   a transaction in block 663195, so that that and all following blocks' hashes
   will be different, triggering a reorg.
-  - `after-large-reorg.txt` contains empty blocks 663151 to 663250, except with
+  - `after-large-reorg.txt` contains empty blocks 663180 to 663250, except with
     a transaction in block 663180, so that a reorg is triggered at that height.
 - `generate.sh` contains a script for generating the input directories to the
   `genblocks` tool, so that these block sets can be easily recreated or
--- basic-reorg/generate.sh
+++ basic-reorg/generate.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
+set -e
 
 # This generates the input to `genblocks` to create block sets for testing small
 # and large (more or less than 10 blocks) reorgs that don't involve any
 # wallet-related transactions. Run this script then use `genblocks` (in
-# lightwalletd) to generate the block file.
+# lightwalletd) to generate the block file. This script should be run with
+# the basic-reorg directory of this repository.
 
 # All-empty blocks for the before-reorg set.
 # The current height is 663200.
 mkdir -p before-reorg
-for i in {663151..663200}; do
+for i in {663150..663200}; do
     touch "./before-reorg/$i.txt"
 done
+genblocks -start-height 663150 -blocks-dir before-reorg/ >before-reorg.txt
+rm -r before-reorg/
 
 # There's a reorg less-than-10 back from current height, plus a bunch of new blocks.
 mkdir -p after-small-reorg
-for i in {663151..663250}; do
+for i in {663195..663250}; do
     touch "./after-small-reorg/$i.txt"
 done
 # Reorg at height 663195.
 echo "0400008085202f8901950521a79e89ed418a4b506f42e9829739b1ca516d4c590bddb4465b4b347bb2000000006a4730440220142920f2a9240c5c64406668c9a16d223bd01db33a773beada7f9c9b930cf02b0220171cbee9232f9c5684eb918db70918e701b86813732871e1bec6fbfb38194f53012102975c020dd223263d2a9bfff2fa6004df4c07db9f01c531967546ef941e2fcfbffeffffff026daf9b00000000001976a91461af073e7679f06677c83aa48f205e4b98feb8d188ac61760356100000001976a91406f6b9a7e1525ee12fd77af9b94a54179785011b88ac4c880b007f880b000000000000000000000000" > ./after-small-reorg/663195.txt
-
+genblocks -start-height 663195 -blocks-dir after-small-reorg/ >after-small-reorg.txt
+rm -r after-small-reorg/
 
 mkdir -p after-large-reorg
-for i in {663151..663250}; do
+for i in {663180..663250}; do
     touch "./after-large-reorg/$i.txt"
 done
 # Reorg at height 663180.
 echo "0400008085202f8901950521a79e89ed418a4b506f42e9829739b1ca516d4c590bddb4465b4b347bb2000000006a4730440220142920f2a9240c5c64406668c9a16d223bd01db33a773beada7f9c9b930cf02b0220171cbee9232f9c5684eb918db70918e701b86813732871e1bec6fbfb38194f53012102975c020dd223263d2a9bfff2fa6004df4c07db9f01c531967546ef941e2fcfbffeffffff026daf9b00000000001976a91461af073e7679f06677c83aa48f205e4b98feb8d188ac61760356100000001976a91406f6b9a7e1525ee12fd77af9b94a54179785011b88ac4c880b007f880b000000000000000000000000" > ./after-large-reorg/663180.txt
+genblocks -start-height 663180 -blocks-dir after-large-reorg/ >after-large-reorg.txt
+rm -r after-large-reorg/
